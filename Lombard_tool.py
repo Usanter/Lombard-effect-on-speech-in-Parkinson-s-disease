1ed31accedd42a158db01e9d72879ebd29cbb1d8
Usanter <trolland@protonmail.ch>
2019-11-06 23:26:16 +0100

    Tool v1 - GUI version + README

--- Lombard_tool.py
+++ Lombard_tool.py
@@ -1,16 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sunday November 3 2019
-@author: Thomas Rolland
-INESC-ID & UZA
-"""
 import os
 import numpy as np
-import scipy.io.wavfile as wav
-import math
 import pyaudio
-import time
-
+from guizero import App, PushButton, Text, Slider, Picture
 # Global constants
 BUFFER_SIZE = 1200 #4200
 CHANNELS = 1
@@ -20,49 +11,92 @@ SAMPLE_RATE = 44100
 HOP_SIZE = BUFFER_SIZE//2
 PERIOD_SIZE_IN_FRAME = HOP_SIZE
 
-if __name__ == '__main__':
-    # Initiating PyAudio object
-    pA = pyaudio.PyAudio()
-    # Open the microphone stream
-    mic = pA.open(format=FORMAT, channels=CHANNELS,
-            rate=SAMPLE_RATE, input=True,
-            frames_per_buffer=PERIOD_SIZE_IN_FRAME)
-    # Open the headset stream
-    stream = pA.open(format=FORMAT,
-                            channels=1,
-                            rate=SAMPLE_RATE,
-                            output=True)
-    volume = 0.5
-    silence_frame = 10
-    # Main loop
-    while not False:
+class LombardTool():
+    def __init__(self):
+        # Variables definition
+        self.volume = 0.1
+        self.silence_frame = 50
+        self.noise_value = 0
+        self.db_value = 0
+        self.state = False #  0 = Pause; 1 = play
+        # ========== GUI =============
+        # Create the GUI app
+        self.app = App(title="Lombard effect on speech in Parkinson's disease App")
+        # Energy detection option
+        self.message = Text(self.app, text="Select threshold for voice detection")
+        self.slider = Slider(self.app,start=0,end=6, command=self.slider_noise)
+        # Noise characteristic option
+        self.message2 = Text(self.app,text="Select noise in dB")
+        self.slider2 = Slider(self.app,start=0,end=10,command=self.slider_db)
+        # Play/pause button
+        self.button = PushButton(self.app,text="Start/Stop", command=self.play_pause)
+        self.state_text = Text(self.app, text="Off")
+        logo1 = Picture(self.app, "./img/UZA.png")
+        logo2 = Picture(self.app, "./img/inesc.png")
+        # ========== Audio ===============
+        # Initialization PyAudio Object
+        pA = pyaudio.PyAudio()
+        # Open microphine and heaset stream
+        self.mic = pA.open(format=FORMAT, channels=CHANNELS,
+                rate=SAMPLE_RATE, input=True,
+                frames_per_buffer=PERIOD_SIZE_IN_FRAME)
+        self.speak = pA.open(format=FORMAT, channels=CHANNELS,
+                rate=SAMPLE_RATE, output=True)
+        # Event loop
+        self.app.repeat(10,self.loop_func) #here we made the loop function every 10ms
 
+        # Display the app
+        self.app.display()
+
+    #def display(self):
+    #    self.app.display()
+
+
+    #def update(self):
+    #    self.app.update()
+
+    # Main loop function
+    def loop_func(self):
+        # If stop then don't do anything
+        if not(self.state):
+            return -1
+        # Otherwise do the white noise generation
         samples = (np.random.random(size=8300)).astype(np.float32)
-        # Send this white noize the the buffer
-        stream.write(volume*samples,PERIOD_SIZE_IN_FRAME)
-        # Listening the imput stream from the microphone
-        input_data = mic.read(PERIOD_SIZE_IN_FRAME, exception_on_overflow=False)
-        # Convert into number that vad can understand it
+        # Send this white noize to the speaker/headset
+        self.speak.write(self.volume * self.noise_value *samples,PERIOD_SIZE_IN_FRAME)
+        # Listen the input steam of the microphone
+        input_data = self.mic.read(PERIOD_SIZE_IN_FRAME,exception_on_overflow=False)
+        # Convert into number that can be understand
         data = np.frombuffer(input_data, dtype=np.float32)
-        # Compute the energy of the buffer
-        vad = np.sum(data**2)/len(data)
-        # If the energy is above 1e5 : Speech
-        if vad > 1e-4:
-            # We increase the volume of the white noize when speech
-            volume = 1
-            silence_frame = 0
+        # Compute the energy of the input
+        nrj = np.sum(data**2)/len(data)
+        # If energy is above the threshold change noize volume
+        if nrj > self.db_value:
+            self.volume = 0.5
+            self.silence_frame = 0
         else:
-            # Otherwise lower the volume
-            volume = 0.1
-            silence_frame += 1
-        if silence_frame < 40:
-            volume = 1
-        print(silence_frame)
-        # Create the white noize
-        #samples = (np.random.random(size=16500)).astype(np.float32)
-        # Send this white noize the the buffer
-        #stream.write(volume*samples)
+            self.volume = 0.001
+            self.silence_frame += 1
+        if self.silence_frame < 10:
+            self.volume = 1
 
-        # Wait
-        #time.sleep(PERIOD_SIZE_IN_FRAME/SAMPLE_RATE)
+    # A simple function to update the start/stop behaviour
+    def play_pause(self):
+        self.state = not(self.state)
+        if self.state == True:
+            self.state_text.value = "On"
+        else:
+            self.state_text.value = "Off"
 
+    # When the slider noise change, update the value
+    def slider_noise(self, slider_value):
+        if int(slider_value == 0):
+            self.db_value =  1
+        else:
+            self.db_value = 0.1**(int(slider_value))
+    # When the slider db change, update the value
+    def slider_db(self, slider_value):
+        self.noise_value = 0.1 * int(slider_value) #10 ** (int(slider_value) / 20)
+
+
+foo = LombardTool()
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Lombard-effect-on-speech-in-Parkinson-s-disease
-This repository is the tools used in the Lombard effect on speech in Parkinson’s disease research
+# Lombard effect on speech in Parkinson's disease tool
+
+This tool is used to carry out the study on the effect of Lombard on Parkinson's patients.
+
+### Installation and use
+
+These tools require python (version 3+) to work.
+
+Install the dependencies and launch the tools.
+
+```sh
+$ git clone https://github.com/Usanter/Lombard-effect-on-speech-in-Parkinson-s-disease.git
+$ git pip install -r requirements.txt
+$ python Lombard_effect_tool.py
+```
+
+The software can be used with 3 buttons:
+- The first slider corresponds to the vocal activity detection (based on the energy of the signal),  higher the value is easier is the threshold
+- The second slider corresponds to the power of the white noise, higher correspond to a more powerfull noise signal
+- The last button is just a Start/Stop button to control the tool
+
+
